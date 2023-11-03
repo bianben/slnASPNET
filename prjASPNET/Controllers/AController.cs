fddf2c6c6b14774576664aec46a3855ad14bf5b1
bian <bute77889@gmail.com>
2023-11-03 09:17:26 +0800

    加入專案檔案。

---
+++ prjASPNET/Controllers/AController.cs
@@ -0,0 +1,52 @@
+﻿using prjMAUIDEMO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjASPNET.Controllers
+{
+    public class AController : Controller
+    {
+        // GET: A
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string demoParameter(int? pid)
+        {
+            if (pid == 1)
+                return "XBox 加入購物車成功";
+            else if (pid == 2)
+                return "PS5 加入購物車成功";
+            return "找不到該產品資料";
+        }
+
+
+        public string demoRequest()
+        {
+            string id = Request.QueryString["pid"];
+
+            if (id == "1")
+                return "XBox 加入購物車成功";
+            else if (id == "2")
+                return "PS5 加入購物車成功";
+            return "找不到該產品資料";
+        }
+        public string demoResponse()
+        {
+            Response.Clear();
+            Response.ContentType = "application/octet-stream";
+            Response.Filter.Close();
+            Response.WriteFile(@"C:\1.ispan\class\12_XAML\slnMauiDEMO\prjMauiDEMO\Resources\Images\fly.jpg");
+            Response.End();
+            return "";
+        }
+        public string lotto()
+        {
+            return new CLotto().pull();
+        }
+    }
+}
