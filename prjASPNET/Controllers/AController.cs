c80194ae3e5ce6d294955ce67ab12ea2fafc14f6
bian <bute77889@gmail.com>
2023-11-06 08:58:42 +0800

    0.0

--- prjASPNET/Controllers/AController.cs
+++ prjASPNET/Controllers/AController.cs
@@ -1,6 +1,8 @@
-﻿using prjMAUIDEMO.Models;
+﻿using prjASPNET.Models;
+using prjMAUIDEMO.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +15,97 @@ namespace prjASPNET.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult bindingCustomerById(int? id)
+        {
+            CCustomer x = null;
+            if (id != null)
+            {
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString = "Data Source=.;Initial Catalog=dbDemo;Integrated Security=True";
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * FROM tCustomer WHERE fId=" + id.ToString(), conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                //if (dr.Read())
+                //{
+                //    ViewBag.II = dr["fId"].ToString();
+                //    ViewBag.KK = dr["fName"].ToString();
+                //    ViewBag.PP = dr["fPhone"].ToString();
+                //    ViewBag.MM = dr["fEmail"].ToString();
+                //}
+
+                if (dr.Read())
+                {
+                    x = new CCustomer()
+                    {
+                        fId = (int)dr["fId"],
+                        fName = dr["fName"].ToString(),
+                        fPhone = dr["fPhone"].ToString(),
+                        fEmail = dr["fEmail"].ToString(),
+                    };
+
+                }
+                conn.Close();
+            }
+            return View(x);
+        }
+
+        public ActionResult displayCustomerById(int? id)
+        {
+            if (id != null)
+            {
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString = "Data Source=.;Initial Catalog=dbDemo;Integrated Security=True";
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * FROM tCustomer WHERE fId=" + id.ToString(), conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                //if (dr.Read())
+                //{
+                //    ViewBag.II = dr["fId"].ToString();
+                //    ViewBag.KK = dr["fName"].ToString();
+                //    ViewBag.PP = dr["fPhone"].ToString();
+                //    ViewBag.MM = dr["fEmail"].ToString();
+                //}
+
+                if (dr.Read())
+                {
+                    CCustomer x = new CCustomer()
+                    {
+                        fId = (int)dr["fId"],
+                        fName = dr["fName"].ToString(),
+                        fPhone = dr["fPhone"].ToString(),
+                        fEmail = dr["fEmail"].ToString(),
+                    };
+                    ViewBag.KK=x;
+                }
+                conn.Close();
+            }
+            return View();
+        }
+
+        public string queryCustomer(int? id)
+        {
+            if (id == null)
+                return "沒有指定id";
+            
+            string s = "沒有符合查詢的資料";
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=.;Initial Catalog=dbDemo;Integrated Security=True";
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM tCustomer WHERE fId=" + id.ToString(), conn);
+            SqlDataReader dr= cmd.ExecuteReader();
+
+            if(dr.Read())
+                s = dr["fName"].ToString()+" / " + dr["fPhone"].ToString();
+
+            conn.Close();
+            return s;
         }
 
         public string demoParameter(int? pid)
