6eeffbb2874b0edcf740764f23026c458ca04e0d
bian <bute77889@gmail.com>
2023-11-06 16:51:25 +0800

    進度

--- prjASPNET/Controllers/AController.cs
+++ prjASPNET/Controllers/AController.cs
@@ -17,88 +17,67 @@ namespace prjASPNET.Controllers
             return View();
         }
 
-        public string testing()
+        public ActionResult demoFrom()
+        {
+            ViewBag.ANS = "X=?";
+            if (!string.IsNullOrEmpty(Request.Form["txtA"]))
+            {
+                int a = Convert.ToInt32(Request.Form["txtA"]);
+                ViewBag.A = a;
+                int b = Convert.ToInt32(Request.Form["txtB"]);
+                ViewBag.B = b;
+                int c = Convert.ToInt32(Request.Form["txtC"]);
+                ViewBag.C = c;
+
+                double d = (b * b) - (4 * a * c);
+                double e = Math.Sqrt(d);
+
+                double f = (-b + e) / (2 * a);
+                double g = (-b - e) / (2 * a);
+                
+
+
+                ViewBag.ANS =$"X={f}或{g}";
+            }
+            return View();
+        }
+
+        public string testingQuery()
+        {
+            return "目前客戶共有:" + (new CCustomerFactory()).queryAll().Count();
+        }
+
+        public string testingUpdete()
+        {
+            CCustomer x = new CCustomer();
+            x.fId = 4;
+            x.fName= "test";
+            x.fPhone = "03-556677";
+            x.fEmail = "1234567890@gmail.com";
+            x.fAddress = "Tao";
+            x.fPassword = "1234567890";
+            (new CCustomerFactory()).update(x);
+            return "修改資料成功";
+        }
+
+        public string testingDelete()
+        {
+            CCustomer x = new CCustomer();
+            (new CCustomerFactory()).delete(1);
+            return "刪除資料成功";
+        }
+
+        public string testingInsert()
         {
             CCustomer x = new CCustomer();
             x.fName = "Lin Chen";
-            x.fPhone = "0980800553";
+            //x.fPhone = "0980800553";
             x.fEmail = "lin@gmail.com";
-            x.fAddress = "Taipei";
+            //x.fAddress = "Taipei";
             x.fPassword = "123456";
 
             (new CCustomerFactory()).create(x);
             return "新增資料成功";
-        }
-
-        public ActionResult bindingCustomerById(int? id)
-        {
-            CCustomer x = null;
-            if (id != null)
-            {
-                SqlConnection conn = new SqlConnection();
-                conn.ConnectionString = "Data Source=.;Initial Catalog=dbDemo;Integrated Security=True";
-                conn.Open();
-
-                SqlCommand cmd = new SqlCommand("SELECT * FROM tCustomer WHERE fId=" + id.ToString(), conn);
-                SqlDataReader dr = cmd.ExecuteReader();
-
-                //if (dr.Read())
-                //{
-                //    ViewBag.II = dr["fId"].ToString();
-                //    ViewBag.KK = dr["fName"].ToString();
-                //    ViewBag.PP = dr["fPhone"].ToString();
-                //    ViewBag.MM = dr["fEmail"].ToString();
-                //}
-
-                if (dr.Read())
-                {
-                    x = new CCustomer()
-                    {
-                        fId = (int)dr["fId"],
-                        fName = dr["fName"].ToString(),
-                        fPhone = dr["fPhone"].ToString(),
-                        fEmail = dr["fEmail"].ToString(),
-                    };
-
-                }
-                conn.Close();
-            }
-            return View(x);
-        }
-
-        public ActionResult displayCustomerById(int? id)
-        {
-            if (id != null)
-            {
-                SqlConnection conn = new SqlConnection();
-                conn.ConnectionString = "Data Source=.;Initial Catalog=dbDemo;Integrated Security=True";
-                conn.Open();
-
-                SqlCommand cmd = new SqlCommand("SELECT * FROM tCustomer WHERE fId=" + id.ToString(), conn);
-                SqlDataReader dr = cmd.ExecuteReader();
-
-                //if (dr.Read())
-                //{
-                //    ViewBag.II = dr["fId"].ToString();
-                //    ViewBag.KK = dr["fName"].ToString();
-                //    ViewBag.PP = dr["fPhone"].ToString();
-                //    ViewBag.MM = dr["fEmail"].ToString();
-                //}
-
-                if (dr.Read())
-                {
-                    CCustomer x = new CCustomer()
-                    {
-                        fId = (int)dr["fId"],
-                        fName = dr["fName"].ToString(),
-                        fPhone = dr["fPhone"].ToString(),
-                        fEmail = dr["fEmail"].ToString(),
-                    };
-                    ViewBag.KK=x;
-                }
-                conn.Close();
-            }
-            return View();
         }
 
         public ActionResult bindingCustomerById(int? id)
--- prjASPNET/Models/CCustomerFactory.cs
+++ prjASPNET/Models/CCustomerFactory.cs
@@ -8,26 +8,150 @@ namespace prjASPNET.Models
 {
     public class CCustomerFactory
     {
+        public CCustomer queryById(int fId)
+        {
+            string sql = "SELECT * FROM tCustomer WHERE fId=@K_FID";
+            List<SqlParameter> paras = new List<SqlParameter> ();
+            paras.Add(new SqlParameter("K_FID", (object)fId));
+            List<CCustomer> list = queryBySql(sql, paras);
+            if (list.Count == 0)
+                return null;
+            return list[0];
+        }
+
+        private List<CCustomer> queryBySql(string sql, List<SqlParameter> paras)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=.;Initial Catalog=dbDemo;Integrated Security=True";
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            if (paras != null)
+                cmd.Parameters.AddRange(paras.ToArray());
+            SqlDataReader dr = cmd.ExecuteReader();
+            List<CCustomer> list = new List<CCustomer>();
+            while (dr.Read())
+            {
+                CCustomer x = new CCustomer()
+                {
+                    fId = (int)dr["fId"],
+                    fName = dr["fName"].ToString(),
+                    fPassword = dr["fPassword"].ToString(),
+                    fEmail = dr["fEmail"].ToString(),
+                    fAddress = dr["fAddress"].ToString(),
+                    fPhone = dr["fPhone"].ToString()
+                };
+                list.Add(x);
+            }
+            conn.Close();
+            return list;
+        }
+
+        public List<CCustomer> queryAll()
+        {
+            string sql = "SELECT * FROM tCustomer";
+            return queryBySql(sql, null);
+        }
+
+        public void update(CCustomer p)
+        {
+            List<SqlParameter> paras = new List<SqlParameter>();
+            string sql = "UPDATE tCustomer SET";
+            if (!string.IsNullOrEmpty(p.fName))
+            {
+                sql += " fName=@K_fNAME,";
+                paras.Add(new SqlParameter("K_FNAME", (object)p.fName));
+            }
+            if (!string.IsNullOrEmpty(p.fPhone))
+            {
+                sql += "fPhone=@K_FPHONE,";
+                paras.Add(new SqlParameter("K_FPHONE", (object)p.fPhone));
+            }
+            if (!string.IsNullOrEmpty(p.fEmail))
+            {
+                sql += "fEmail=@K_FEMAIL,";
+                paras.Add(new SqlParameter("K_FEMAIL", (object)p.fEmail));
+            }
+            if (!string.IsNullOrEmpty(p.fAddress))
+            {
+                sql += "fAddress=@K_FADDRESS,";
+                paras.Add(new SqlParameter("K_FADDRESS", (object)p.fAddress));
+            }
+            if (!string.IsNullOrEmpty(p.fPassword))
+            {
+                sql += "fPassword=@K_FPASSWORD,";
+                paras.Add(new SqlParameter("K_FPASSWORD", (object)p.fPassword));
+            }
+            if (sql.Trim().Substring(sql.Trim().Length - 1, 1) == ",")
+                sql = sql.Trim().Substring(0, sql.Trim().Length - 1);
+            sql += " WHERE fId=@K_FID";
+            paras.Add(new SqlParameter("K_FID", (object)p.fId));            
+            executeSql(sql, paras);
+        }
+        public void delete(int? fId)
+        {
+            string sql = "DELETE FROM tCustomer WHERE fId=@K_FID";
+            List<SqlParameter> paras = new List<SqlParameter>();
+            paras.Add(new SqlParameter("K_FID", (object)fId));
+            executeSql(sql, paras);
+        }
+
         public void create(CCustomer p)
         {
+            List<SqlParameter> paras = new List<SqlParameter>();
             string sql = "INSERT INTO tCustomer(";
-            sql += "fName,";
-            sql += "fPhone,";
-            sql += "fEmail,";
-            sql += "fAddress,";
-            sql += "fPassword,";
+            if(!string.IsNullOrEmpty(p.fName))
+                sql += "fName,";
+            if (!string.IsNullOrEmpty(p.fPhone))
+                sql += "fPhone,";
+            if (!string.IsNullOrEmpty(p.fEmail))
+                sql += "fEmail,";
+            if (!string.IsNullOrEmpty(p.fAddress))
+                sql += "fAddress,";
+            if (!string.IsNullOrEmpty(p.fPassword))
+                sql += "fPassword,";
+            if (sql.Trim().Substring(sql.Trim().Length - 1, 1) == ",")
+                sql = sql.Trim().Substring(0, sql.Trim().Length - 1);
             sql += ")VALUES(";
-            sql += "'"+p.fName+"',";
-            sql += "'" + p.fPhone + "',";
-            sql += "'" + p.fEmail + "',";
-            sql += "'" + p.fAddress + "',";
-            sql += "'" + p.fPassword + "',";
+            if (!string.IsNullOrEmpty(p.fName))
+            {
+                sql += "@K_FNAME,";
+                paras.Add(new SqlParameter("K_FNAME", (object)p.fName));
+            }
+            if (!string.IsNullOrEmpty(p.fPhone))
+            {
+                sql += "@K_FPHONE,";
+                paras.Add(new SqlParameter("K_FPHONE", (object)p.fPhone));
+            }
+            if (!string.IsNullOrEmpty(p.fEmail))
+            {
+                sql += "@K_FEMAIL,";
+                paras.Add(new SqlParameter("K_FEMAIL", (object)p.fEmail));
+            }
+            if (!string.IsNullOrEmpty(p.fAddress))
+            {
+                sql += "@K_FADDRESS,";
+                paras.Add(new SqlParameter("K_FADDRESS", (object)p.fAddress));
+            }
+            if (!string.IsNullOrEmpty(p.fPassword))
+            {
+                sql += "@K_FPASSWORD,";
+                paras.Add(new SqlParameter("K_FPASSWORD", (object)p.fPassword));
+            }
+            if (sql.Trim().Substring(sql.Trim().Length - 1, 1) == ",")
+                sql = sql.Trim().Substring(0, sql.Trim().Length - 1);
+            sql += ")";
+            executeSql(sql, paras);
+        }
 
+        private static void executeSql(string sql,List<SqlParameter>paras)
+        {
             SqlConnection conn = new SqlConnection();
             conn.ConnectionString = "Data Source=.;Initial Catalog=dbDemo;Integrated Security=True";
             conn.Open();
 
             SqlCommand cmd = new SqlCommand(sql, conn);
+            if (paras != null)
+                cmd.Parameters.AddRange(paras.ToArray());
             cmd.ExecuteNonQuery();
         }
     }
