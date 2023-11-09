99f01823d7248f37781d7a5f8d91fc3590045aad
bian <bute77889@gmail.com>
2023-11-09 19:25:01 +0800

    進度

---
+++ prjASPNET/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using prjASPNET.Models;
+using prjASPNET.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjASPNET.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            if (Session[CDictionary.SK_LIGINED_USER] == null)
+                return RedirectToAction("Login");
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(CLoginViewModel vm)
+        {
+            CCustomer user = (new CCustomerFactory()).queryByEmail(vm.txtAccount);
+            if (user != null)
+            {
+                if (user.fPassword.Equals(vm.txtPassword))
+                {
+                    Session[CDictionary.SK_LIGINED_USER] = user;
+                    return RedirectToAction("Index");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ prjASPNET/Models/CDictionary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prjASPNET.Models
+{
+    public class CDictionary
+    {
+        public static readonly string SK_PURCHASED_PRODUCTS_LIST = "SK_PURCHASED_PRODUCTS_LIST";
+        public static readonly string SK_LIGINED_USER = "SK_LIGINED_USER";
+    }
+}
